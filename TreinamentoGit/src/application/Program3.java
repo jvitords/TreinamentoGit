bb22b8609139f5e810b862c69dcd86425f0299e1
João Vitor Santos <jvduarte.santos@hotmail.com>
2025-02-19 16:29:29 -0300

    Add 1 pra ver erro no git

---
+++ TreinamentoGit/src/application/Program3.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Program 3");
+		System.out.println("Teste pra ver se dá erro no git ao fazer o push");
+	}
+
+}
