f7d4760c742a5693392a24045a59a2433e796a57
João Vitor Santos <jvduarte.santos@hotmail.com>
2025-02-20 14:54:12 -0300

    add o program 2

--- TreinamentoGit/src/application/Program.java
+++ TreinamentoGit/src/application/Program.java
@@ -3,11 +3,7 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-
 		System.out.println("Program");
-		
 		System.out.println("Program 2");
-
 	}
-
 }
--- TreinamentoGit/src/application/Program3.java
+++ TreinamentoGit/src/application/Program3.java
@@ -3,15 +3,8 @@ package application;
 public class Program3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
 		System.out.println("Program 3");
 		System.out.println("Teste pra ver se dá erro no git ao fazer o push");
-
 		System.out.println("Sou do local");
-
-		
 	}
-
 }
